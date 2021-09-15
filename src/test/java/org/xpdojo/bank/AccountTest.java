514700f1c53d82800556f4197161443f527705a9
Abhishek.Sinha@db.com <Abhishek.sinha@db.com>
2021-09-15 17:32:11 +0530

    test file

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -3,13 +3,33 @@ package org.xpdojo.bank;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.Assert.assertThat;
+
 
 public class AccountTest {
 
     @Test
-	@Disabled
     public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+
+        Account account = new Account();
+        account.deposit(10);
+        assertThat(account.balance,is(100));
     }
+
+    @Test
+    public void openingAccount() {
+
+        Account account = new Account();
+        account.deposit(10);
+        assertThat(account.balance,is(10));
+    }
+
+    @Test
+    public void withdrawal(){
+
+    }
+
+
+
 }
